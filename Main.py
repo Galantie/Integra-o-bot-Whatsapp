90cfc893a30434c9075d7e6034b410359f5b006d
Galantie <46659392+Galantie@users.noreply.github.com>
2019-02-03 19:48:26 -0200

    prontinho

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,13 +13,12 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(executable_path=r"C:\Program Files (x86)\Google\chromedriver.exe")
 driverbot = webdriver.Chrome(executable_path=r"C:\Program Files (x86)\Google\chromedriver.exe")
 driver.get('https://web.whatsapp.com/')
-driverbot.get(
-    'https://assistant-us-south.watsonplatform.net/us-south/crn:v1:bluemix:public:conversation:us-south:a~2F3f86fa858fa14ff9858aeb981d42afdd:7ca225fd-0469-41d9-8c7c-03fb2d31901c::/workspaces/f66e3f3c-0675-4924-b44f-23bff592f886/build/intents')
+driverbot.get('https://assistant-us-south.watsonplatform.net/us-south/crn:v1:bluemix:public:conversation:us-south:a~2F3f86fa858fa14ff9858aeb981d42afdd:7ca225fd-0469-41d9-8c7c-03fb2d31901c::/workspaces/f66e3f3c-0675-4924-b44f-23bff592f886/build/intents')
 x = 0
 
 iniciar = input('iniciar( o qr deve ser escaneado e a conversa com o bot iniciada) :')
 
-name = 'Vitoria'
+name = 'Mãe'
 msg_whats = driverbot.find_element_by_id('-chatPanel_exchange{}_part0'.format(x))
 mensagem = msg_whats.text
 msg = str(mensagem)
@@ -26,16 +27,19 @@ msg = str(mensagem)
 
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
-iniciar = input('depois de clicar no contato prosseguir?')
+time.sleep(3)
+#iniciar = input('depois de clicar no contato prosseguir?')
 
 msg_box = driver.find_element_by_class_name('_2S1VP')
 msg_box.send_keys(msg)
-iniciar = input('Enviar?')
+time.sleep(3)
+#iniciar = input('Enviar?')
 #WebDriverWait(driver, 30)
 button = driver.find_element_by_class_name('_35EW6')
 button.click()
 
-iniciar = input('seguir para a espera pela mensagem?')
+time.sleep(3)
+#iniciar = input('seguir para a espera pela mensagem?')
 #driver.find_element_by_class_name('_1aTxu').click()
 
 x=1
@@ -56,14 +60,16 @@ while (True):
         #posicao_chat = '-chatPanel_exchange{}_part0'.format(x)
 
         #novoelement = wait.until(EC.presence_of_element_located((By.ID, '-chatPanel_exchange{}_part0'.format(x))))
-        input('Foi gerada a mensagem?')
+        #input('Foi gerada a mensagem?')
+        time.sleep(3)
         msg_whats = driverbot.find_element_by_id('-chatPanel_exchange{}_part0'.format(x))
         x=x+1
         mensagem = msg_whats.text
         msg = str(mensagem)
         msg_box = driver.find_element_by_class_name('_2S1VP')
         msg_box.send_keys(msg)
-        iniciar = input('Enviar?')
+        time.sleep(3)
+        #iniciar = input('Enviar?')
         # WebDriverWait(driver, 30)
         button = driver.find_element_by_class_name('_35EW6')
         button.click()
